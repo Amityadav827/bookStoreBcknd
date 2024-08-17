bbb8fd8972ac678c0e54d1da19b258c23847810d
Amit Yadav <amityadav98255@gmail.com>
2024-08-17 16:21:23 +0530

    ddd

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ['https://bookstorebcknd.onrender.com'],
+  origin: '*',
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
